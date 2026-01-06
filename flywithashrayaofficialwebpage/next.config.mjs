6d2cce62f59cdcc1fe31c7aa55ce1067af395079
AmLpR06 <amalrajan8509@gmail.com>
2026-01-06 13:17:42 +0530

    add plus unsplash

--- flywithashrayaofficialwebpage/next.config.mjs
+++ flywithashrayaofficialwebpage/next.config.mjs
@@ -9,6 +9,11 @@ const nextConfig = {
       },
       {
         protocol: "https",
+        hostname: "plus.unsplash.com",
+        pathname: "/**",
+      },
+      {
+        protocol: "https",
         hostname: "www.holidify.com",
         pathname: "/**",
       },
