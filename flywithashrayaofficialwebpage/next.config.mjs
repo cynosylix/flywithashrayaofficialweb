716eb680a4c605d36a5d2787d53d947b71bc4700
AmLpR06 <amalrajan8509@gmail.com>
2026-01-06 13:13:02 +0530

    fix the nextconfig page

--- flywithashrayaofficialwebpage/next.config.mjs
+++ flywithashrayaofficialwebpage/next.config.mjs
@@ -1,6 +1,5 @@
-import type { NextConfig } from "next";
-
-const nextConfig: NextConfig = {
+/** @type {import('next').NextConfig} */
+const nextConfig = {
   images: {
     remotePatterns: [
       {
@@ -11,6 +10,11 @@ const nextConfig: NextConfig = {
       {
         protocol: "https",
         hostname: "www.holidify.com",
+        pathname: "/**",
+      },
+      {
+        protocol: "https",
+        hostname: "randomuser.me",
         pathname: "/**",
       },
     ],
